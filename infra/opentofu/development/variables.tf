810cd0e8266b4745225d2f9c1f04f4f6285a9828
Temi ✨ <a.adenuga@hotmail.com>
2026-08-05 05:56:01 +0100

    chore(infra): finalize the development AWS bootstrap

--- infra/opentofu/development/variables.tf
+++ infra/opentofu/development/variables.tf
@@ -4,16 +4,6 @@ variable "aws_region" {
   default     = "eu-west-2"
 }
 
-variable "aws_account_id" {
-  description = "AWS development member account ID"
-  type        = string
-
-  validation {
-    condition     = can(regex("^[0-9]{12}$", var.aws_account_id))
-    error_message = "aws_account_id must be a 12-digit AWS account ID"
-  }
-}
-
 variable "github_oidc_subject_prefix" {
   description = "Immutable GitHub repository subject prefix trusted by the development contract role"
   type        = string
@@ -34,7 +24,7 @@ variable "budget_alert_email" {
 variable "monthly_budget_usd" {
   description = "Monthly development account budget in USD"
   type        = number
-  default     = 10
+  default     = 5
 
   validation {
     condition     = var.monthly_budget_usd >= 1
